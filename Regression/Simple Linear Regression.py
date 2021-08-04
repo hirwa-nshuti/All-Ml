98ee5f699f6fb7211d7e3e0017a220937edc987f
hirwa-nshuti <hirwanshutiflx@gmail.com>
2021-08-04 22:31:38 +0200

    Added numpy functions

--- Regression/Simple Linear Regression.py
+++ Regression/Simple Linear Regression.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+import numpy
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.metrics import r2_score
@@ -25,7 +25,7 @@ print('Coefficients: ', model.coef_)
 print('The Intercept: ', model.intercept_)
 
 # Reshaping y array to fit the line
-y = np.repeat(y[..., np.newaxis], 3, -1)
+y = numpy.repeat(y[..., numpy.newaxis], 3, -1)
 
 regressionLine = model.coef_ * x + model.intercept_
 
@@ -35,6 +35,6 @@ plt.plot(x, regressionLine)
 plt.show()
 
 y_hat = model.predict(x_test)
-print('Mean absolute error: %.2f' % np.mean(np.absolute(y_hat - y_test)))
-print('Residual sum of squares(MSE): %.2f' % np.mean((y_hat - y_test) ** 2))
+print('Mean absolute error: %.2f' % numpy.mean(numpy.absolute(y_hat - y_test)))
+print('Residual sum of squares(MSE): %.2f' % numpy.mean((y_hat - y_test) ** 2))
 print('R_2 score: %.2f' % r2_score(y_hat, y_test))
